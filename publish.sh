cd59599375c6c46904fa44a230b94673dc67668e
jacobagerson <jacobagerson@gmail.com>
2026-05-30 14:08:23 -0400

    Update

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git add .
-git commit -m "Site update: improved typography and tactile project cards"
+git commit -m "Site update: $(date)"
 git push origin master
 echo "🚀 Changes pushed! Site will update in ~1 minute."
